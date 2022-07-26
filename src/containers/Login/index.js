053f05923035b1ded55b72f18e6d2e1196f16c52
cc <calliechen@Callies-MacBook.local>
2022-07-25 20:56:10 -0400

    used antd to beautify form

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,32 +1,40 @@
 import { useState } from 'react';
+import { Button, Input, Form, Dialog} from 'antd-mobile';
 import './index.css';
+const initialValues={
+  username:'hahaha',
+  password:'12345',
+}
 
 const Login = () => {
-  const [name, setName] = useState('')
-  const [pwd, setPwd] = useState('')
-
-
-  const clickHandler = () => {
-    alert('Login Succesful' + name + ',' +pwd);
-
+  const [form] = Form.useForm()
+  const onSubmit = () => {
+    const values = form.getFieldValue()
+    Dialog.alert({
+      content:JSON.stringify(values),
+    })
   }
 
-
-  const onChangeNameHandler = (e) => {
-    setName(e.target.value);
-  }
-
-  const onChangePwHandler = (e) => {
-    setPwd(e.target.value);
-
-  }
 
   return (
     <div className="login">
-        <div>Username:<input onChange={onChangeNameHandler}/></div>
-        <div>Password:<input type="password" onChange={onChangePwHandler}/></div>
-        <div><button onClick={clickHandler}>login</button></div>
 
+      <Form 
+        form={form}
+        layout='horizontal' mode='card' initialValues={initialValues}
+        footer={
+          <Button block color='primary' onClick={onSubmit} size='large'>
+            Login
+          </Button>
+        }
+      >
+          <Form.Item label='Username' name='username'>
+            <Input placeholder='pls enter usename' clearable />
+          </Form.Item>
+          <Form.Item label='Password' name='password'>
+            <Input placeholder='pls enter password' clearable type='password'/>
+          </Form.Item>
+        </Form>
     </div>
   );
 }
